31c310520a30fd354c6cdfa5b95a36b220cd354d
Mike Pettman <mp_palmatum@yahoo.com>
2017-12-27 15:30:02 -0600

    prepped for mongo

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ const thingObj = {
     third: 'dwarf'
 };
 
+// establishes MongoDB connection
+// mongoose.connect('localhost:/27017/mocha-chai-test');
+
+// const recipeSchema = new mongoose.Schema({
+//     name: String,
+//     timeInMin: Number,
+//     ingredients: Number
+// });
+
+// app.get('/recipes', function(req, res){
+//     recipeSchema.find({}, function(err, results){
+//         if (err){
+//             console.log('Get error: ', err);
+//             res.sendStatus(500);
+//         } else {
+//             res.send(results);
+//         }
+//     })
+// });
+
 app.get('/', function(req, res){
     res.send(thingObj);
 });
@@ -15,6 +35,14 @@ app.get('/', function(req, res){
 app.post('/', function(req, res){
     let object = req.body;
     // do something with object
+    // newThing.save(function(err){
+    //     if (err){
+    //         console.log('update user error: ', err);
+    //         res.sendStatus(500);
+    //     } else {
+    //         res.sendStatus(202);
+    //     }
+    // });
     res.sendStatus(202);
 });
 
